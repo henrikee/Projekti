3a069261d8fb0240c564e9d48b68fa6f765c0b52
henrikee <55734779+henrikee@users.noreply.github.com>
2020-03-24 16:20:03 +0200

    meikkaus

--- Main.php
+++ Main.php
@@ -1,12 +1,13 @@
 <?php
 include("includes/navMain.php");
+include("config/https.php")
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="nav.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>Document</title>
 </head>
