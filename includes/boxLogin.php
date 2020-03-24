1181482ff63003e0311af553cb4fd31ae6704096
tmstrnn <58732662+tmstrnn@users.noreply.github.com>
2020-03-24 15:45:04 +0200

    tässä pikku muutos.mm,,

---
+++ includes/boxLogin.php
@@ -0,0 +1,7 @@
+<div class="boxLogin">
+<label for="username">Käyttäjätunnus</label>
+<input type="text" id="username" name="username"><br><br>
+<label for="pw">Salasana</label>
+<input type="password" id="password" name="pw"><br><br>
+<input class="btn" type="submit" name="loginUser"   value="Kirjaudu"/>
+</div>  
---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+include("includes/navLogin.php");
+include("includes/boxLogin.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <title>Document</title>
+</head>
+<body>
+</body>
+</html>
