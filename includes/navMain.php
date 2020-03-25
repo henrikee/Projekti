b07671b5b58bc75f3d64b74498201ce02c1d70cd
henrikee <55734779+henrikee@users.noreply.github.com>
2020-03-25 12:17:43 +0200

    php

--- includes/navMain.php
+++ includes/navMain.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/nav.css">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <title>LEEROOYY.JENKIS</title>
-</head>
-<body>
-    <!-- Luodaan nav html johon laitoin linkit, otsikon sekä burgerin(auttaa skaalaamisessa) -->
-    <nav>
+<!-- Luodaan nav html johon laitoin linkit, otsikon sekä burgerin(auttaa skaalaamisessa) -->
+<nav>
         <input type="checkbox" id="check">
         <label for="check" class="checkbtn">
             <i class="fas fa-bars"></i>
@@ -23,5 +13,3 @@
             <li><a href="#">Kirjaudu ulos</a></li>
         </ul>
     </nav> 
-</body>
-</html>
