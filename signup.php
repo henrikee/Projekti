7df7c4615ffd4641adf11eb26210f1c55a664bf1
tmstrnn <58732662+tmstrnn@users.noreply.github.com>
2020-03-25 11:31:15 +0200

    jo

--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@ include("includes/navSignup.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href= "signup.css">
+    <link rel="stylesheet" type="text/css" href= "css/signup.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>Document</title>
 </head>
