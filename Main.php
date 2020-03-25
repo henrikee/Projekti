ff3361c20b91c3db1a51c008372f50bf6407e9ea
henrikee <55734779+henrikee@users.noreply.github.com>
2020-03-25 14:40:10 +0200

    pläjäys

--- Main.php
+++ Main.php
@@ -1,13 +1,14 @@
 <?php
 include("includes/navMain.php");
-include("config/https.php")
+include("config/https.php");
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="css/nav.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>Document</title>
 </head>
---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<?php
+include_once("config/https.php");
+include_once("config/config.php");
+session_start();
+?>
+<!DOCTYPE html> 
+<html  lang="fi-FI">
+<head>
+<title> Jenkins</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
