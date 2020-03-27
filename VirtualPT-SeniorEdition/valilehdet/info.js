182b349990f7f67dcc54535f82acfc1bcd974af8
heinj <58248995+heinj@users.noreply.github.com>
2020-03-27 22:51:59 +0200

    joo

---
+++ VirtualPT-SeniorEdition/valilehdet/info.js
@@ -0,0 +1,43 @@
+var date = new Date();
+document.getElementById("date").innerHTML = "Tänään on "+date.getDate()+'.'+(date.getMonth()+1)+'.'+date.getFullYear();
+
+function func1(){
+    var x = document.getElementById("p1");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    }else{
+    x.style.display = "none";
+    }
+}
+function func2(){
+    var x = document.getElementById("p2");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    }else{
+    x.style.display = "none";
+    }
+}
+function func3(){
+    var x = document.getElementById("p3");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    }else{
+    x.style.display = "none";
+    }
+}
+function func4(){
+    var x = document.getElementById("p4");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    }else{
+    x.style.display = "none";
+    }
+}
+function func5(){
+    var x = document.getElementById("p5");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    }else{
+    x.style.display = "none";
+    }
+}
