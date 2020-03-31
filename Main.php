d2dcfd0f9541cc87a870088fd897cef09822e61a
heinj <58248995+heinj@users.noreply.github.com>
2020-03-31 21:20:12 +0300

    fdfdf

--- Main.php
+++ Main.php
@@ -1,6 +1,6 @@
 <?php
-include("includes/navMain.php");
-include("includes/header.php");
+include("../includes/navMain.php");
+include("../includes/header.php");
 
 ?>
 <!DOCTYPE html>
---
+++ includes/navInfo.php
@@ -0,0 +1,18 @@
+<!-- Luodaan nav html johon laitoin linkit, otsikon sekä burgerin(auttaa skaalaamisessa) -->
+<nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo">Virtual PT</label>
+        <ul>
+            <li><a href="#">Etusivu</a></li>
+            <li><a href="../html/diary.php">Päiväkirja</a></li>
+            <li><a href="">Liikunta</a></li>
+            <li><a class="active" href="#">Tietopankki</a></li>
+            <li><a href="logOutUser.php">Kirjaudu ulos</a></li>
+        </ul>
+        <?php
+        echo("<p>** User: " .$_SESSION['suserName']. " " . $_SESSION['suserEmail']);
+        ?>
+</nav> 
