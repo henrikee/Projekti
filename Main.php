f8f1a96e07cced869aeecd391caf6462a9713e86
heinj <58248995+heinj@users.noreply.github.com>
2020-04-07 17:19:37 +0300

    juuuuuuuuu

--- Main.php
+++ Main.php
@@ -11,7 +11,7 @@ include("includes/header.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/main.css">
     <!-- Valmis ikoni (w3 school) -->
     <!-- Ready to use icon (w3 school) -->
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
