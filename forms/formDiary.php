c9af323b42e952242820259337cf1ff9655f1fe9
tmstrnn <58732662+tmstrnn@users.noreply.github.com>
2020-04-14 17:04:24 +0300

    formi

--- forms/formDiary.php
+++ forms/formDiary.php
@@ -1,53 +1,55 @@
-<form method="post">
-    <div class="rendered-form"><div class="">
-    <label class="formbuilder-text-label">Mitä kuuluu?<br></label>
-    <input type="text" class="form-control" access="false" title="Anna lyhyt kuvaus siitä, miten tänään meni." name="undefined"></div>
-    <div class=""><label " class="formbuilder-text-label">Mitä liikuntaa harrastit tänään?<br>
-    </label>
-            <input type="text" class="form-control" access="false" name="undefined"></div><div class="">
-                <label class="formbuilder-radio-group-label">Liikunnan kesto<br></label>
-<div class="radio-group">
-    <div class="formbuilder-radio">
-    <input access="false" name="radioAnswer" type="radio">
-    <label>alle 30 min</label>
+<form method="post"><div>
+        <div>
+        <label>Päivämäärä tänään</label><br>
+        <input type="date"><br>
+        <label>Mitä liikuntaa harrastit tänään?<br></label>
+        <input type="text" class="form-control" access="false">
+        </div>
+<div>
+<label>Liikunnan kesto<br></label>
+        <div class="radio-group">
+                <div>
+            <input name="radEx" access="false" id="radEx-0" type="radio" checked="unchecked">
+                <label>alle 30 min</label>
+                 </div>
+        <div>
+        <input name="radEx" access="false" id="radEx-1" type="radio">
+        <label for="radEx-1">30-60 min</label>
+        </div>
+                  <div>
+        <input name="radEx" access="false" id="radEx-2" type="radio">
+        <label for="radEx-2">yli 60 min</label>
+                  </div>
+         </div>
+    </div>
+<div>
+<label >Syke treenin jälkeen<br></label>
+<input type="text" class="form-control" access="false">
+</div>
+<div class="">
+<label >Söitkö terveellisesti tänään? Mitä söit?<br></label>
+<input type="text" class="form-control" access="false">
+</div>
+<div class="">
+<label>Miten nukuit viime yönä? <br></label>
+<input type="text" class="form-control" access="false" >
+</div>
+<div><label >Unen kesto viime yönä<br></label>
+<div>
+<div>
+<input access="false" name="RadSleep" type="radio" checked="checked">
+<label>alle 5h</label>
+</div>
+<div ><input access="false" name="RadSleep" type="radio">
+<label >6-8 h</label>
 </div>
 <div class="formbuilder-radio">
-        <input access="false" name="radioAnswer" value="option-2" type="radio">
-        <label>30-60 min</label>
-    </div>
-    <div class="formbuilder-radio">
-            <input access="false" name="radioAnswer" value="option-3" type="radio" checked="checked">
-            <label>yli 60 min</label>
-        </div>
-    </div>
+<input access="false" name="RadSleep" type="radio">
+<label >yli 8h</label>
 </div>
-     <div>
-         <label class="formbuilder-text-label">Mikä oli sykkeesi treenin jälkeen?<br></label>
-        <input type="text" class="form-control" access="false"  name="undefined">
-    </div>
-    <div>
-        <label class="formbuilder-text-label">Mitä söit tänään?<br></label>
-        <input type="text" class="form-control" access="false"  name="undefined">
-    </div>
-    <div>
-        <label class="formbuilder-text-label">Nukuitko hyvin viime yönä?<br></label>
-        <input type="text" class="form-control" access="false" name="undefined">
-    </div>
-        <div>
-            <label class="formbuilder-radio-group-label">Kuinka paljon nukuit?<br></label>
-            <div class="radio-group">
-                <div class="formbuilder-radio"><input name="radioAnswer" access="false" value="option-1" type="radio">
-                <label >alle 5 tuntia</label>
-            </div>
-            <div class="formbuilder-radio">
-                <input access="false"name="radioAnswer" value="option-2" type="radio">
-                <label>6-8 tuntia</label>
-            </div>
-            <div class="formbuilder-radio">
-                    <input access="false"name="radioAnswer"  value="option-3" type="radio">
-                    <label >yli 8 tuntia</label>
-                </div>
-            </div>
-        </div>
-    </div>
+</div>
+</div>
+<button type="submit" name="submitEx" class="btn" value="Tallenna">Tallenna</button>
+<button type="submit" name="ResetEx" class="btn" value="Peruuta">Peruuta</button>
+</div>
 </form>
