636b2b128257bae8d4c137d80dc817d96b4aae07
tmstrnn <58732662+tmstrnn@users.noreply.github.com>
2020-04-14 12:53:38 +0300

    aa

---
+++ forms/formDiary.php
@@ -0,0 +1,53 @@
+<form method="post">
+    <div class="rendered-form"><div class="">
+    <label class="formbuilder-text-label">Mitä kuuluu?<br></label>
+    <input type="text" class="form-control" access="false" title="Anna lyhyt kuvaus siitä, miten tänään meni." name="undefined"></div>
+    <div class=""><label " class="formbuilder-text-label">Mitä liikuntaa harrastit tänään?<br>
+    </label>
+            <input type="text" class="form-control" access="false" name="undefined"></div><div class="">
+                <label class="formbuilder-radio-group-label">Liikunnan kesto<br></label>
+<div class="radio-group">
+    <div class="formbuilder-radio">
+    <input access="false" name="radioAnswer" type="radio">
+    <label>alle 30 min</label>
+</div>
+<div class="formbuilder-radio">
+        <input access="false" name="radioAnswer" value="option-2" type="radio">
+        <label>30-60 min</label>
+    </div>
+    <div class="formbuilder-radio">
+            <input access="false" name="radioAnswer" value="option-3" type="radio" checked="checked">
+            <label>yli 60 min</label>
+        </div>
+    </div>
+</div>
+     <div>
+         <label class="formbuilder-text-label">Mikä oli sykkeesi treenin jälkeen?<br></label>
+        <input type="text" class="form-control" access="false"  name="undefined">
+    </div>
+    <div>
+        <label class="formbuilder-text-label">Mitä söit tänään?<br></label>
+        <input type="text" class="form-control" access="false"  name="undefined">
+    </div>
+    <div>
+        <label class="formbuilder-text-label">Nukuitko hyvin viime yönä?<br></label>
+        <input type="text" class="form-control" access="false" name="undefined">
+    </div>
+        <div>
+            <label class="formbuilder-radio-group-label">Kuinka paljon nukuit?<br></label>
+            <div class="radio-group">
+                <div class="formbuilder-radio"><input name="radioAnswer" access="false" value="option-1" type="radio">
+                <label >alle 5 tuntia</label>
+            </div>
+            <div class="formbuilder-radio">
+                <input access="false"name="radioAnswer" value="option-2" type="radio">
+                <label>6-8 tuntia</label>
+            </div>
+            <div class="formbuilder-radio">
+                    <input access="false"name="radioAnswer"  value="option-3" type="radio">
+                    <label >yli 8 tuntia</label>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
